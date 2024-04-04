4e60b0ac4f97dcaf81fffbbce8a7f04c4824fadd
xunhaoL <143568774+xunhaoL@users.noreply.github.com>
2024-04-03 20:54:10 -0500

    a

--- dagster_quickstart/io.py
+++ dagster_quickstart/io.py
@@ -4,6 +4,10 @@ import zipfile
 
 import pandas as pd
 import requests
+import csv
 
-def read_single_csv(csv_path: pathlib.Path) -> pd.DataFrame:
-    return pd.read_csv(csv_path)
+def read_csv_from_zip(zip_file_path: pathlib.Path, csv_path: pathlib.Path) -> pd.DataFrame:
+    # Open the ZIP file
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        # Extract the CSV file
+        return zip_ref.extractall()
--- test/test_all.py
+++ test/test_all.py
@@ -3,6 +3,6 @@ from pathlib import Path
 import pandas as pd
 from dagster_quickstart import io
 
-def test_read_single_csv(tmpdir):
-    contributions = io.read_single_csv("data/rideshare_kaggle.csv")
+def read_csv_from_zip_test(tmpdir):
+    contributions = io.read_csv_from_zip("data/archive.zip","rideshare_kaggle.csv")
     assert contributions.size == 39505047
