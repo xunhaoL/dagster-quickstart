1f382522c3b5abf39cc592a720f6cb44f010f925
xunhaoL <143568774+xunhaoL@users.noreply.github.com>
2024-04-03 21:17:41 -0500

    b

--- dagster_quickstart/io.py
+++ dagster_quickstart/io.py
@@ -6,8 +6,18 @@ import pandas as pd
 import requests
 import csv
 
-def read_csv_from_zip(zip_file_path: pathlib.Path, csv_path: pathlib.Path) -> pd.DataFrame:
-    # Open the ZIP file
+def read_csv_from_zip(zip_file_path: pathlib.Path, csv_filename: str) -> pd.DataFrame:
+    # Extract the CSV file from the ZIP archive
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-        # Extract the CSV file
-        return zip_ref.extractall()
+        csv_file_list = [file for file in zip_ref.namelist() if file.endswith('.csv')]
+        if not csv_file_list:
+            raise ValueError("No CSV file found in the ZIP archive")
+        if len(csv_file_list) > 1:
+            raise ValueError("Multiple CSV files found in the ZIP archive, specify the exact filename")
+
+        csv_filename_in_zip = csv_file_list[0]
+        with zip_ref.open(csv_filename_in_zip) as csv_file:
+            # Read the CSV file into a DataFrame
+            df = pd.read_csv(csv_file)
+
+    return df
